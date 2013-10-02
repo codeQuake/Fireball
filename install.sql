a06653a2b347f38cd6c6e39d93b98044665410d7
Jens Krumsieck <info@codequake.de>
2013-10-02 09:17:20 +0200

    section types III

--- install.sql
+++ install.sql
@@ -36,16 +36,10 @@ cssID VARCHAR (255),
 cssClasses VARCHAR(255)
 );
 
---section_type
-DROP TABLE IF EXISTS cms1_content_section_type;
-CREATE TABLE cms1_content_section_type(
-sectionTypeID INT (10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(255),
-className VARCHAR(255)
-);
+
 
 --foreign keys
 ALTER TABLE cms1_page ADD FOREIGN KEY (userID) REFERENCES wcf1_user (userID) ON DELETE SET NULL;
 ALTER TABLE cms1_content ADD FOREIGN KEY (pageID) REFERENCES cms1_page (pageID) ON DELETE SET NULL;
 ALTER TABLE cms1_content_section ADD FOREIGN KEY (contentID) REFERENCES cms1_content (contentID) ON DELETE SET NULL;
-ALTER TABLE cms1_content_section ADD FOREIGN KEY (sectionTypeID) REFERENCES cms1_content_section_type (sectionTypeID) ON DELETE SET NULL;
+ALTER TABLE cms1_content_section ADD FOREIGN KEY (sectionTypeID) REFERENCES wcf1_object_type (objectTypeID) ON DELETE SET NULL;
