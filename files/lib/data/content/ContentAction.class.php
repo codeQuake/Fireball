403176a6449d5e653c9a9babe066df3a344c6c3f
Jens Krumsieck <info@codequake.de>
2013-10-11 11:46:46 +0200

    deleting childs

--- files/lib/data/content/ContentAction.class.php
+++ files/lib/data/content/ContentAction.class.php
@@ -3,9 +3,28 @@ namespace cms\data\content;
 
 use wcf\data\AbstractDatabaseObjectAction;
 use wcf\system\exception\UserInputException;
+use cms\data\content\section\ContentSectionAction;
 
 class ContentAction extends AbstractDatabaseObjectAction{
 
     protected $className = 'cms\data\content\ContentEditor';
+    protected $permissionsDelete = array('admin.cms.content.canAddContent');
+    protected $requireACP = array('delete');
+    
+    public function delete(){
+    
+        //delete all sections beloning to the contents
+        foreach($this->objectIDs as $objectID){
+            $content = new Content($objectID);
+            $list = $content->getSections();
+            $sectionIDs = array();
+            foreach($list as $section){
+                $sectionIDs[] = $section->sectionID;
+            }
+            $action = new ContentSectionAction($sectionIDs, 'delete', array());
+            $action->executeAction();
+        }
+        parent::delete();
+    }
    
 }
--- files/lib/data/page/PageAction.class.php
+++ files/lib/data/page/PageAction.class.php
@@ -1,12 +1,28 @@
 <?php
 namespace cms\data\page;
-
 use wcf\data\AbstractDatabaseObjectAction;
 use wcf\system\exception\UserInputException;
+use cms\data\content\ContentAction;
 
 class PageAction extends AbstractDatabaseObjectAction{
 
-   protected $className = 'cms\data\page\PageEditor';
-   protected $permissionsDelete = array('admin.cms.page.canAddPage');
-   protected $requireACP = array('delete');
+    protected $className = 'cms\data\page\PageEditor';
+    protected $permissionsDelete = array('admin.cms.page.canAddPage');
+    protected $requireACP = array('delete');
+   
+    public function delete(){
+    
+        //delete all contents beloning to the pages
+        foreach($this->objectIDs as $objectID){
+            $page = new Page($objectID);
+            $list = $page->getContentList();
+            $contentIDs = array();
+            foreach($list as $content){
+                $contentIDs[] = $content->contentID;
+            }
+            $action = new ContentAction($contentIDs, 'delete', array());
+            $action->executeAction();
+        }
+        parent::delete();
+    }
 }
