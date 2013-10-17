67760eb7469656399f490952d58d41dc0ac96eec
Jens Krumsieck <info@codequake.de>
2013-10-17 14:44:50 +0200

    kills layouts when updateing sheets

--- files/lib/data/stylesheet/StylesheetAction.class.php
+++ files/lib/data/stylesheet/StylesheetAction.class.php
@@ -1,10 +1,34 @@
 <?php
 namespace cms\data\stylesheet;
 use wcf\data\AbstractDatabaseObjectAction;
+use cms\system\layout\LayoutHandler;
+use cms\data\layout\LayoutList;
 
 class StylesheetAction extends AbstractDatabaseObjectAction{
 
     protected $className = 'cms\data\stylesheet\StylesheetEditor';
     protected $permissionsDelete = array('admin.cms.stylesheet.canAddStylesheet');
     protected $requireACP = array('delete');
+    
+    public function delete(){
+        parent::delete();
+        
+        //kill all layouts
+        $layoutList = new LayoutList();
+        $layoutList->readObjects();
+        foreach($layoutList->getObjects() as $layout){
+            LayoutHandler::getInstance()->deleteStylesheet($layout->layoutiD);
+        }
+    }
+    
+    public function update(){
+        parent::update();
+        
+        //kill all layouts
+        $layoutList = new LayoutList();
+        $layoutList->readObjects();
+        foreach($layoutList->getObjects() as $layout){
+            LayoutHandler::getInstance()->deleteStylesheet($layout->layoutID);
+        }
+    }
 }
