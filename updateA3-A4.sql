29e575fbed74b2c1b4ba60db3a19816e85218f3e
Jens Krumsieck <info@codequake.de>
2013-12-12 18:06:58 +0100

    obsolete

--- updateA3-A4.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE cms1_page ADD sidebarOrientation ENUM('left', 'right') NOT NULL DEFAULT 'right' AFTER showSidebar;
-
-ALTER TABLE cms1_content ADD position ENUM('body', 'sidebar') NOT NULL DEFAULT 'body' AFTER cssClasses; 
