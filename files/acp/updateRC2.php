c4b36a642633a90b4ca23c142e39884ec8b30938
Jens Krumsieck <info@codequake.de>
2014-01-25 14:23:17 +0100

    obsolete

--- files/acp/updateRC2.php
+++
@@ -1,6 +0,0 @@
-<?php
-use wcf\system\WCF;
-
-$sql = "ALTER TABLE cms".WCF_N."_content ADD type ENUM('div', 'ul', 'ol') NOT NULL DEFAULT 'div'";
-$statement = WCF::getDB()->prepareStatement($sql);
-$statement->execute();
