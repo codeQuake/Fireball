7cf19c729a65ecd6b7fb50c880df59f41e50a081
Jens Krumsieck <info@codequake.de>
2014-02-09 18:08:18 +0100

    sql

--- files/acp/update.php
+++ files/acp/update.php
@@ -1,10 +1,15 @@
 <?php
+use wcf\system\WCF;
 
-$sql = "CREATE TABLE cms1_counter(  
+$sql = "CREATE TABLE cms1_counter(
                                     time INT (10) NOT NULL,
+                                    userID INT(10),
                                     referrer VARCHAR(255),
                                     browser VARCHAR(255),
                                     browserVersion VARCHAR(255),
                                     resolution VARCHAR(255),
                                     ipAddress VARCHAR(255)
-);";
+        );";
+        
+$statement = WCF::getDB()->prepareStatement($sql);
+WCF::getDB()->execute($statement, array());
