4d3895f92fc7eff339160a133babc74adaa415d5
Jens Krumsieck <info@codequake.de>
2014-03-03 14:43:36 +0100

    auto delete

--- optionalpackages/de.codequake.cms.exporter/files_cms/lib/system/cronjob/BackupCronjob.class.php
+++ optionalpackages/de.codequake.cms.exporter/files_cms/lib/system/cronjob/BackupCronjob.class.php
@@ -4,6 +4,7 @@ use wcf\data\cronjob\Cronjob;
 use wcf\system\cronjob\AbstractCronjob;
 
 use cms\data\restore\RestoreAction;
+use cms\data\restore\RestoreList;
 use cms\system\export\CMSExportHandler;
 
 class BackupCronjob extends AbstractCronjob{
@@ -15,6 +16,14 @@ class BackupCronjob extends AbstractCronjob{
                 $data = array('filename' => $filename,
                               'time' => TIME_NOW);
                 $action = new RestoreAction(array(), 'create', array('data' => $data));
+                $action->executeAction();
+            }
+            if(CMS_AUTOMATIC_BACKUP_DELETE != 0){
+                $deleteTime = (time() - (CMS_AUTOMATIC_BACKUP_DELETE * 86400));
+                $list = new RestoreList();
+                $list->getConditionBuilder()->add('time <= ?', array($deleteTime));
+                $list->readObjects();
+                $action = new RestoreAction($list->getObjects(), 'delete');
                 $action->executeAction();
             }
             
