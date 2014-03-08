a6463d3b5d48122bdd4fd6ee77c3d7005ba8a3f3
Jens Krumsieck <info@codequake.de>
2014-03-08 17:28:20 +0100

    added feed plugin

---
+++ optionalpackages/de.codequake.cms.news.rss/files/lib/data/feed/FeedAction.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace cms\data\feed;
+use wcf\data\AbstractDatabaseObjectAction;
+
+/**
+ * @author	Jens Krumsieck
+ * @copyright	2014 codeQuake
+ * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
+ * @package	de.codequake.cms.news.rss
+ */
+
+class FeedAction extends AbstractDatabaseObjectAction{
+
+    protected $className = 'cms\data\feed\FeedEditor';
+    protected $permissionsDelete = array('admin.cms.file.canAddFeed');
+    protected $requireACP = array('delete');
+    
+   
+}
