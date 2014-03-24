1546884c730329ea7408e8c4eac6c250c963e6e8
Jens Krumsieck <info@codequake.de>
2014-03-24 11:28:08 +0100

    CMSPageMenuItemProvider

--- files/lib/system/menu/page/CMSPageMenuItemProvider.class.php
+++ files/lib/system/menu/page/CMSPageMenuItemProvider.class.php
@@ -1,13 +1,20 @@
 <?php
 namespace cms\system\menu\page;
 use wcf\system\menu\page\DefaultPageMenuItemProvider;
+use cms\data\page\Page;
 
 class CMSPageMenuItemProvider extends DefaultPageMenuItemProvider{
     
     protected $page = null;
+    
+    public function getPage(){
+        $tmp = explode("=", $this->getDecoratedObject()->menuItemLink);
+        $this->page =  new Page(intval($tmp[1]));
+    }
 
 	public function getLink(){
-        if($this->page === null) return parent::getLink();
-        $this->page->getLink();
+        $this->getPage();
+        if($this->page === null || $this->page->pageID == 0) return parent::getLink();
+        return $this->page->getLink();
     }   
 }
