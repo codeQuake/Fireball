c513e01cdf1959120c082399e097018612c74584
Jens Krumsieck <info@codequake.de>
2014-05-05 16:51:38 +0200

    obsolete!

--- update1_2.sql
+++
@@ -1,8 +0,0 @@
---update structre
-ALTER TABLE cms1_page ADD availableDuringOfflineMode TINYINT(1) NOT NULL DEFAULT 0 AFTER invisible;
-ALTER TABLE cms1_page ADD alias VARCHAR(255) NOT NULL DEFAULT '' AFTER pageID;
-
-ALTER TABLE cms1_page ADD FOREIGN KEY (parentID) REFERENCES cms1_page (pageID) ON DELETE SET NULL;
-
-
-ALTER TABLE cms1_news ADD showSignature TINYINT(1) NOT NULL DEFAULT 0 AFTER enableBBCodes;
