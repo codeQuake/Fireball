5c8fcd30e7b1e1fa47bfdb75dfbe42dca9f753a7
Jens Krumsieck <info@codequake.de>
2014-05-13 16:56:35 +0200

    setting menu provider foreach page in CMS automatically via pagemenu forms

--- files/lib/system/content/type/HeadlineContentType.class.php
+++ files/lib/system/content/type/HeadlineContentType.class.php
@@ -5,11 +5,10 @@ use cms\data\content\Content;
 use wcf\system\WCF;
 
 /**
- *
- * @author Jens Krumsieck
- * @copyright codeQuake 2014
- * @package de.codequake.cms
- * @license GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
+ * @author	Jens Krumsieck
+ * @copyright	codeQuake 2014
+ * @package	de.codequake.cms
+ * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
  */
 class HeadlineContentType extends AbstractContentType {
 
---
+++ files/lib/system/event/listener/PageMenuListener.class.php
@@ -0,0 +1,20 @@
+<?php
+namespace cms\system\event\listener;
+use wcf\data\package\PackageCache;
+use wcf\system\event\IEventListener;
+
+/**
+ * @author	Jens Krumsieck
+ * @copyright	codeQuake 2014
+ * @package	de.codequake.cms
+ * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
+ */
+
+class PageMenuListener implements IEventListener {
+	public function execute($eventObj, $className, $eventName) {
+		if ($eventObj->menuItemController == 'cms\page\PagePage') {
+			$eventObj->additionalFields['className'] = 'cms\system\menu\page\CMSPageMenuItemProvider';
+			$eventObj->additionalFields['packageID'] = PackageCache::getInstance()->getPackageID('de.codequake.cms');
+		}
+	}
+}
