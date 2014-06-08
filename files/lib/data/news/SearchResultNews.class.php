6a821d4dabf8e91c6c25af05b73e99735894e59a
Jens Krumsieck <info@codequake.de>
2014-06-08 15:57:20 +0200

    search fix

--- files/lib/data/news/SearchResultNews.class.php
+++ files/lib/data/news/SearchResultNews.class.php
@@ -24,7 +24,7 @@ class SearchResultNews extends ViewableNews implements ISearchResultObject {
 
 	public function getLink($query = '') {
 		if ($query) {
-			return LinkHandler::getInstance()->getLink('Link', array(
+			return LinkHandler::getInstance()->getLink('News', array(
 				'application' => 'cms',
 				'object' => $this->getDecoratedObject(),
 				'highlight' => urlencode($query)
--- templates/galleryContentTypeOutput.tpl
+++ templates/galleryContentTypeOutput.tpl
@@ -1,5 +1,7 @@
-<ul>
-	{foreach from=$images item=image}
-		<li><img src="{$image->getURL()}" alt="" class="jsResizeImage" /></li>
-	{/foreach}
-</ul>
+<div class="slideshowContainer">
+	<ul>
+		{foreach from=$images item=image}
+			<li><img src="{$image->getURL()}" alt="" class="jsResizeImage" /></li>
+		{/foreach}
+	</ul>
+</div>
