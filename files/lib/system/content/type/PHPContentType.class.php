595f9b008a31de5d8c095dc5721c3e93834770ef
Jens Krumsieck <info@codequake.de>
2014-06-08 16:32:33 +0200

    php type experimental

---
+++ files/lib/system/content/type/PHPContentType.class.php
@@ -0,0 +1,26 @@
+<?php
+namespace cms\system\content\type;
+
+use cms\data\content\Content;
+/**
+ * @author	Jens Krumsieck
+ * @copyright	codeQuake 2014
+ * @package	de.codequake.cms
+ * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
+ */
+class PHPContentType extends AbstractContentType {
+
+	protected $icon = 'icon-code';
+
+	public $objectType = 'de.codequake.cms.content.type.php';
+
+	public function getFormTemplate() {
+		return 'phpContentType';
+	}
+
+	public function getOutput(Content $content) {
+		$data = $content->handleContentData();
+		$php = substr($data['text'], 4);
+		return eval($php);
+	}
+}
