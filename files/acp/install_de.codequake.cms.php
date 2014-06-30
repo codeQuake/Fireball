10705a41ff976beeffc8de4824b0016585e6670b
Jens Krumsieck <info@codequake.de>
2014-06-30 16:20:34 +0200

    separated news system The separatíon of Fireball's News-System is now successful. The Package is valid and i found no errors.

--- files/acp/install_de.codequake.cms.php
+++ files/acp/install_de.codequake.cms.php
@@ -12,15 +12,10 @@ use wcf\system\WCF;
  */
 $package = $this->installation->getPackage();
 
-// default values
-DashboardHandler::setDefaultValues('de.codequake.cms.news.newsList', array(
-	'de.codequake.cms.latestNews' => 1
-));
-
 // install date
 $sql = "UPDATE	wcf" . WCF_N . "_option
-    SET	optionValue = ?
-    WHERE	optionName = ?";
+	SET	optionValue = ?
+	WHERE	optionName = ?";
 $statement = WCF::getDB()->prepareStatement($sql);
 $statement->execute(array(
 	TIME_NOW,
