f1c4932da34248c41953b6bcc20728a584d9ebb4
Jens Krumsieck <info@codequake.de>
2014-07-10 07:56:57 +0200

    backuphandler

--- files/lib/system/backup/BackupHandler.class.php
+++ files/lib/system/backup/BackupHandler.class.php
@@ -7,6 +7,7 @@ use cms\data\file\FileList;
 use cms\data\folder\FolderList;
 use cms\data\layout\LayoutList;
 use cms\data\stylesheet\StylesheetList;
+use wcf\data\object\type\ObjectTypeCache;
 use wcf\system\io\TarWriter;
 use wcf\system\SingletonFactory;
 use wcf\util\DirectoryUtil;
@@ -22,6 +23,7 @@ use wcf\util\XMLWriter;
 
 class BackupHandler extends SingletonFactory{
 
+	public $objects = array('page', 'content', 'stylesheet', 'layout','file', 'folder');
 	protected $pages = null;
 	protected $contents = null;
 	protected $layouts = null;
@@ -30,6 +32,7 @@ class BackupHandler extends SingletonFactory{
 	protected $folders = null;
 
 	protected  $filename = '';
+	protected $data;
 
 	protected function init() {
 		$this->pages = PageCacheBuilder::getInstance()->getData(array(), 'pages');
@@ -62,15 +65,15 @@ class BackupHandler extends SingletonFactory{
 		//start doc
 		$xml = new XMLWriter();
 		$xml->beginDocument('data', '', '');
-		$objects = array('page', 'content', 'stylesheet', 'layout','file', 'folder');
 
-		foreach ($objects as $object) {
+		foreach ($this->objects as $object) {
 			if ($this->{$object.'s'} !== null && !empty($this->{$object.'s'})) {
 				$xml->startElement($object.'s');
 				foreach ($this->{$object.'s'} as $$object) {
 					$xml->startElement($object);
 					foreach ($$object->getData() as $key => $data) {
-						$xml->writeElement($key, $data);
+						if ($key == 'contentTypeID') $xml->writeElement($key, ObjectTypeCache::getInstance()->getObjectType($data)->objectType);
+						else $xml->writeElement($key, $data);
 					}
 					$xml->endElement();
 				}
@@ -102,4 +105,78 @@ class BackupHandler extends SingletonFactory{
 		$tar->add($files->getFiles(), '', CMS_DIR . 'files/');
 		$tar->create();
 	}
+
+	public function handleImport($filename) {
+		//clean all
+		foreach ($this->objects as $object) {
+			$actionName = ucfirst($object).'Action';
+			$action = new $actionName($this->{$object.'s'}, 'delete');
+			$action->executeAction();
+		}
+
+		if (file_exists(CMS_DIR . 'files/')) {
+			DirectoryUtil::getInstance(CMS_DIR . 'files/')->removeAll();
+		}
+
+		$this->openTar($filename);
+
+		//import all
+		foreach ($this->objects as $object) {
+			if (isset($this->data[$object.'s'])) {
+				foreach($this->data[$object.'s'] as $$object) {
+					$actionName = ucfirst($object).'Action';
+					$action = new $actionName(array(), 'create', array('data' => $$object));
+					$action->executeAction();
+				}
+			}
+		}
+	}
+
+	protected function openTar($filename) {
+		$tar = new Tar($filename);
+		$this->data = $this->readXML($tar);
+		$this->importFiles();
+		$tar->close();
+	}
+
+	protected function importFiles() {
+		$files = 'files.tar';
+		if ($tar->getIndexByFileName($files) === false) {
+			throw new SystemException("Unable to find required file '" . $files . "' in the import archive");
+		}
+		$tar->extract($files, CMS_DIR . 'files/files.tar');
+
+		$ftar = new Tar(CMS_DIR . 'files/files.tar');
+		$contentList = $ftar->getContentList();
+		foreach ($contentList as $key => $val) {
+			if ($val['type'] == 'file' && $val['filename'] != '/files.tar' && $val['filename'] != 'files.tar') $ftar->extract($key, CMS_DIR . 'files/' . $val['filename']);
+			else if (! file_exists(CMS_DIR . 'files/' . $val['filename'])) mkdir(CMS_DIR . 'files/' . $val['filename']);
+		}
+		$ftar->close();
+		@unlink(CMS_DIR . 'files/files.tar');
+	}
+
+	protected function readXML($tar) {
+		$xml = 'cmsData.xml';
+		if ($tar->getIndexByFileName($xml) === false) {
+			throw new SystemException("Unable to find required file '" . $xml . "' in the import archive");
+		}
+		$xmlData = new XML();
+		$xmlData->loadXML($xml, $tar->extractToString($tar->getIndexByFileName($xml)));
+		$xpath = $xmlData->xpath();
+		$root = $xpath->query('/ns:data')->item(0);
+		$items = $xpath->query('child::*', $root);
+		$data = array();
+		foreach ($items as $item) {
+			foreach ($xpath->query('child::*', $item) as $child){
+				foreach ($xpath->query('child::*', $item) as $property){
+					if ($property->tagName == 'contentTypeID') $data[$item->tagName][][$property->tagName] = ObjectTypeCache::getInstance()->getObjectTypeByName($property->nodeValue)->objectType;
+					else $data[$item->tagName][][$property->tagName] = $property->nodeValue;
+				}
+			}
+		}
+
+		return $data;
+	}
 }
+
