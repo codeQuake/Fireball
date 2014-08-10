a85e61aea0cc1c5690d6183f4b387fd8fedfdd75
ray176 <ray176@me.com>
2014-08-10 23:05:03 +0200

    page option types

---
+++ files/lib/system/option/CMSPageSelectOptionType.class.php
@@ -0,0 +1,52 @@
+<?php
+namespace cms\system\option;
+use cms\data\page\PageCache;
+use cms\data\page\PageNodeTree;
+use wcf\data\option\Option;
+use wcf\system\exception\UserInputException;
+use wcf\system\option\AbstractOptionType;
+use wcf\system\WCF;
+
+/**
+ * Option type implementation for cms page selection.
+ * 
+ * @author	Florian Frantzen
+ * @copyright	2014 codeQuake
+ * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
+ * @package	de.codequake.cms
+ */
+class CMSPageSelectOptionType extends AbstractOptionType {
+	/**
+	 * @see	\wcf\system\option\IOptionType::getData()
+	 */
+	public function getData(Option $option, $newValue) {
+		return intval($newValue);
+	}
+
+	/**
+	 * @see	\wcf\system\option\IOptionType::getFormElement()
+	 */
+	public function getFormElement(Option $option, $value) {
+		$nodeTree = new PageNodeTree();
+		$nodeList = $nodeTree->getIterator();
+
+		WCF::getTPL()->assign(array(
+			'nodeList' => $nodeList,
+			'option' => $option,
+			'value' => $value
+		));
+
+		return WCF::getTPL()->fetch('pageSelectOptionType', 'cms');
+	}
+
+	/**
+	 * @see	\wcf\system\option\IOptionType::validate()
+	 */
+	public function validate(Option $option, $newValue) {
+		if (!empty($newValue)) {
+			if (PageCache::getInstance()->getPage($newValue) === null) {
+				throw new UserInputException($option->optionName, 'validationFailed');
+			}
+		}
+	}
+}
