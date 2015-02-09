275de676bd658c97466614c8dea11528c581d7eb
Jens Krumsieck <jenso@codequake.de>
2015-02-09 11:33:34 +0100

    fix update routine (hopefully)

--- files/acp/update_de.codequake.cms_beta8.php
+++ files/acp/update_de.codequake.cms_beta8.php
@@ -1,4 +1,6 @@
 <?php
+use wcf\data\category\CategoryAction;
+use wcf\system\category\CategoryHandler;
 use wcf\system\WCF;
 
 /**
@@ -8,16 +10,42 @@ use wcf\system\WCF;
  * @package	de.codequake.cms
  */
 
-$sql = "SELECT * FROM wcf".WCF_N."_folder";
+//add default category
+$objectType = CategoryHandler::getInstance()->getObjectTypeByName('de.codequake.cms.file');
+$objectAction = new CategoryAction(array(), 'create', array(
+		'data' => array(
+				'description' => '',
+				'isDisabled' => 0,
+				'objectTypeID' => $objectTypeID,
+				'parentCategoryID' => null,
+				'showOrder' => null,
+				'title' => 'default'
+			)	
+		));
+$objectAction->executeAction();
+$returnValues = $objectAction->getReturnValues();
+$categoryID = $returnValues['returnValues']->categoryID;
+
+//move files out of their old subfolders
+$sql = "SELECT * FROM cms".WCF_N."_folder";
 $statement = WCF::getDB()->prepareStatement($sql);
 $statement->execute();
+$files = array();
 while ($row = $statement->fetchArray()) {
-	$dir = opendir(CMS_DIR.'images/'.$row['folderPath']);
+	$files[] = array([$row['fileID']], $categoryID);
+	$dir = opendir(CMS_DIR.'files/'.$row['folderPath']);
 	while (($file = readdir($dir)) !== false) {
 		if ($file != '.' && $file != '..') {
-			copy($dir.'/'.$file, CMS_DIR.'images/'.$file);
+			@copy($dir.'/'.$file, CMS_DIR.'files/'.$file);
 		}
 	}
 	closedir($dir);
 	@unlink($dir);
+}
+
+//get files into basic category
+foreach($files as $file) {
+	$sql = "INSERT INTO cms".CMS_N."_file_to_category VALUES (?, ?)";
+	$statement = WCF::getDB()->prepareStatement($sql);
+	$statement->execute($file);
 }
--- update_beta8.sql
+++ update_beta8.sql
@@ -5,8 +5,7 @@ ALTER TABLE cms1_page DROP showSidebar;
 ALTER TABLE cms1_page ADD allowSubscribing TINYINT(1) NOT NULL DEFAULT 1 AFTER isCommentable;
 ALTER TABLE cms1_page DROP stylesheets;
 
-ALTER TABLE cms1_stylesheet CHANGE sheetID stylesheetID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY;
-ALTER TABLE cms1_file DROP folderID;
+ALTER TABLE cms1_stylesheet CHANGE sheetID stylesheetID INT(10) NOT NULL AUTO_INCREMENT;
 
 DROP TABLE IF EXISTS cms1_folder;
 
@@ -17,6 +16,14 @@ CREATE TABLE cms1_stylesheet_to_page (
 
 	PRIMARY KEY (stylesheetID, pageID)
 );
+
+
+ALTER TABLE cms1_file DROP folderID;
+ALTER TABLE cms1_file DROP filename;
+ALTER TABLE cms1_file CHANGE size fileSize INT(10) NOT NULL DEFAULT 0;
+ALTER TABLE cms1_file CHANGE type fileType VARCHAR(255) NOT NULL DEFAULT '';
+ALTER TABLE cms1_file ADD fileHash VARCHAR(40) NOT NULL DEFAULT '';
+ALTER TABLE cms1_file ADD uploadTime INT(10) NOT NULL DEFAULT 0;
 
 DROP TABLE IF EXISTS cms1_file_to_category;
 CREATE TABLE cms1_file_to_category (
