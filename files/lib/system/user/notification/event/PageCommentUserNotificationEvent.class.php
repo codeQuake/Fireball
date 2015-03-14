0c77d5f0a025a7727a291cbc327c03f1fb8c4a13
mutec <florian@mysterycode.de>
2015-03-14 03:36:41 +0100

    stacked notifications (untestet)

--- files/lib/system/user/notification/event/PageCommentUserNotificationEvent.class.php
+++ files/lib/system/user/notification/event/PageCommentUserNotificationEvent.class.php
@@ -15,49 +15,95 @@ use wcf\system\user\notification\event\AbstractUserNotificationEvent;
  */
 class PageCommentUserNotificationEvent extends AbstractUserNotificationEvent {
 	/**
+	 * @see	\wcf\system\user\notification\event\AbstractUserNotificationEvent::$stackable
+	 */
+	protected $stackable = true;
+	
+	/**
 	 * @see	\wcf\system\user\notification\event\IUserNotificationEvent::getTitle()
 	 */
 	public function getTitle() {
-		return $this->getLanguage()->getDynamicVariable('cms.page.comment.notification.title', array(
-			'page' => PageCache::getInstance()->getPage($this->userNotificationObject->pageID)
-		));
+		$count = count($this->getAuthors());
+		if ($count > 1) {
+			return $this->getLanguage()->getDynamicVariable('cms.page.comment.notification.title.stacked', array(
+				'count' => $count,
+				'timesTriggered' => $this->notification->timesTriggered
+			));
+		}
+		
+		return $this->getLanguage()->get('cms.page.comment.notification.title');
 	}
-
+	
 	/**
 	 * @see	\wcf\system\user\notification\event\IUserNotificationEvent::getMessage()
 	 */
 	public function getMessage() {
 		$comment = new Comment($this->userNotificationObject->commentID);
 		$page = PageCache::getInstance()->getPage($this->userNotificationObject->objectID);
-
+		
+		$authors = $this->getAuthors();
+		if (count($authors) > 1) {
+			if (isset($authors[0])) {
+				unset($authors[0]);
+			}
+			$count = count($authors);
+			
+			return $this->getLanguage()->getDynamicVariable('cms.page.comment.notification.message.stacked', array(
+				'page' => $page,
+				'author' => $this->author,
+				'authors' => array_values($authors),
+				'count' => $count,
+				'others' => $count - 1,
+				'guestTimesTriggered' => $this->notification->guestTimesTriggered
+			));
+		}
+		
 		return $this->getLanguage()->getDynamicVariable('cms.page.comment.notification.message', array(
-			'author' => $this->author,
-			'page' => $page,
-			'userNotificationObject' => $this->userNotificationObject
+				'page' => $page,
+				'author' => $this->author
 		));
 	}
-
+	
 	/**
 	 * @see	\wcf\system\user\notification\event\IUserNotificationEvent::getEmailMessage()
 	 */
 	public function getEmailMessage($notificationType = 'instant') {
 		$comment = new Comment($this->userNotificationObject->commentID);
 		$page = PageCache::getInstance()->getPage($this->userNotificationObject->objectID);
-
+		
+		$authors = $this->getAuthors();
+		if (count($authors) > 1) {
+			if (isset($authors[0])) {
+				unset($authors[0]);
+			}
+			$count = count($authors);
+			
+			return $this->getLanguage()->getDynamicVariable('cms.page.comment.notification.mail.stacked', array(
+				'page' => $page,
+				'author' => $this->author,
+				'authors' => array_values($authors),
+				'count' => $count,
+				'others' => $count - 1,
+				'notificationType' => $notificationType,
+				'guestTimesTriggered' => $this->notification->guestTimesTriggered
+			));
+		}
+		
 		return $this->getLanguage()->getDynamicVariable('cms.page.comment.notification.mail', array(
-			'author' => $this->author,
 			'page' => $page,
-			'userNotificationObject' => $this->userNotificationObject
+			'comment' => $comment,
+			'author' => $this->author,
+			'notificationType' => $notificationType
 		));
 	}
-
+	
 	/**
 	 * @see	\wcf\system\user\notification\event\IUserNotificationEvent::getLink()
 	 */
 	public function getLink() {
 		$comment = new Comment($this->userNotificationObject->commentID);
 		$page = PageCache::getInstance()->getPage($this->userNotificationObject->objectID);
-
+		
 		return $page->getLink();
 	}
 }
