6434155bad11422f7e3a7d0acc5512d08cec1203
Florian Frantzen <ray176@me.com>
2015-03-19 17:18:15 +0100

    save user id instead of username in user contents

--- files/lib/system/content/type/UserContentType.class.php
+++ files/lib/system/content/type/UserContentType.class.php
@@ -2,12 +2,15 @@
 namespace cms\system\content\type;
 
 use cms\data\content\Content;
+use wcf\data\user\User;
 use wcf\data\user\UserProfile;
 use wcf\system\exception\UserInputException;
+use wcf\system\exception\SystemException;
+use wcf\system\request\RequestHandler;
 use wcf\system\WCF;
 
 /**
- * @author	Jens Krumsieck
+ * @author	Jens Krumsieck, Florian Frantzen
  * @copyright	2013 - 2015 codeQuake
  * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
  * @package	de.codequake.cms
@@ -25,17 +28,61 @@ class UserContentType extends AbstractContentType {
 		if (!isset($data['name']) || $data['name'] == '') {
 			throw new UserInputException('data[name]');
 		}
-		if (!UserProfile::getUserProfileByUsername($data['name'])) {
+
+		$userProfile = User::getUserByUsername($data['name']);
+
+		if (!$userProfile) {
 			throw new UserInputException('data[name]', 'notValid');
 		}
+
+		// save user id instead of username
+		$contentData = &RequestHandler::getInstance()->getActiveRequest()->getRequestObject()->contentData;
+		$contentData['userID'] = $userProfile->userID;
+		unset($contentData['name']);
+	}
+
+	/**
+	 * @see \cms\system\content\type\IContentType::getFormTemplate()
+	 */
+	public function getFormTemplate() {
+		$username = '';
+		$contentData = &RequestHandler::getInstance()->getActiveRequest()->getRequestObject()->contentData;
+
+		if (isset($contentData['name'])) {
+			$username = $contentData['name'];
+		}
+		else if (isset($contentData['userID'])) {
+			$userID = $contentData['userID'];
+			$user = new User($userID);
+
+			if ($user->userID) {
+				$username = $user->username;
+			}
+		}
+
+		WCF::getTPL()->assign(array(
+			'username' => $username
+		));
+
+		return parent::getFormTemplate();
 	}
 
 	/**
 	 * @see	\cms\system\content\type\IContentType::getOutput()
 	 */
 	public function getOutput(Content $content) {
+		if ($content->userID) {
+			$user = UserProfile::getUserProfile($content->userID);
+		}
+		else if ($content->name) {
+			$user = UserProfile::getUserProfileByUsername($content->name);
+		}
+		else {
+			throw new SystemException('Neither user id nor username provided');
+		}
+
 		WCF::getTPL()->assign(array(
-			'user' => UserProfile::getUserProfileByUsername($content->name)
+			'user' => $user
 		));
 
 		return parent::getOutput($content);
