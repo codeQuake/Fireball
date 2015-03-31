557d4412f6f9859c0b763b8bca4e41caf308bf6b
Jens Krumsieck <jenso@codequake.de>
2015-03-31 14:06:29 +0200

    started update script We only support an update from 2.0.0 RC 1 to keep it easy ;)

---
+++ files/acp/update_2.0.php
@@ -0,0 +1,50 @@
+<?php
+use cms\data\content\ContentList;
+use cms\data\content\ContentEditor;
+use wcf\data\object\type\ObjectTypeCache;
+
+$multiColumn = ObjectTypeCache::getInstance()->getObjectTypeIDByName('de.codequake.cms.content.type', 'de.codequake.cms.content.type.columns');
+
+$list = new ContentList();
+$list->readObjects();
+
+foreach ($list->getObjects() as $content) {
+	$width = array();
+	
+	//two columns
+	if ($content->getTypeName() == 'de.codequake.cms.content.type.twocolumns') {
+		$width = array(
+					substr($content->width, 0, 2),
+					substr($content->width, 2, 2)
+					);
+	}
+	//three columns
+	else if ($content->getTypeName() == 'de.codequake.cms.content.type.threecolumns') {
+		$width = array(
+					substr($parent->width, 0, 2),
+					substr($parent->width, 2, 2),
+					substr($parent->width, 4, 2)
+					);
+	}
+	
+	//four columns
+	else if ($content->getTypeName() == 'de.codequake.cms.content.type.fourcolumns') {
+		$width = array(
+					substr($parent->width, 0, 2),
+					substr($parent->width, 2, 2),
+					substr($parent->width, 4, 2),
+					substr($parent->width, 6, 2)
+					);
+	}
+	
+	//check if it's a multicolumn type
+	if (!empty($width)) {
+		$columnData = array('columnData' => array($width));
+		$update = array(
+			'contentTypeID' => $multicolumn->objectTypeID,
+			'contentData' => serialize($columnData)
+		);
+		$editor = new ContentEditor($content);
+		$edtior->update($update);
+	}
+}
