723b776b4d25261d16cb238561ba5629233cc2d2
Florian Frantzen <ray176@me.com>
2015-04-07 13:23:33 +0200

    fix update from 2.0.0
    Old object types were deleted before the update script tried to change
    them to the new columns content type

--- files/acp/update_2.0.php
+++ files/acp/update_2.0.php
@@ -10,34 +10,33 @@ $list->readObjects();
 
 foreach ($list->getObjects() as $content) {
 	$width = array();
-	
-	//two columns
+
+	// two columns
 	if ($content->getTypeName() == 'de.codequake.cms.content.type.twocolumns') {
 		$width = array(
-					substr($content->width, 0, 2),
-					substr($content->width, 2, 2)
-					);
+			substr($content->width, 0, 2),
+			substr($content->width, 2, 2)
+		);
 	}
 	//three columns
 	else if ($content->getTypeName() == 'de.codequake.cms.content.type.threecolumns') {
 		$width = array(
-					substr($parent->width, 0, 2),
-					substr($parent->width, 2, 2),
-					substr($parent->width, 4, 2)
-					);
+			substr($parent->width, 0, 2),
+			substr($parent->width, 2, 2),
+			substr($parent->width, 4, 2)
+		);
 	}
-	
 	//four columns
 	else if ($content->getTypeName() == 'de.codequake.cms.content.type.fourcolumns') {
 		$width = array(
-					substr($parent->width, 0, 2),
-					substr($parent->width, 2, 2),
-					substr($parent->width, 4, 2),
-					substr($parent->width, 6, 2)
-					);
+			substr($parent->width, 0, 2),
+			substr($parent->width, 2, 2),
+			substr($parent->width, 4, 2),
+			substr($parent->width, 6, 2)
+		);
 	}
-	
-	//check if it's a multicolumn type
+
+	// check if it's a multicolumn type
 	if (!empty($width)) {
 		$columnData = array('columnData' => array($width));
 		$update = array(
