c23bfe142f11f56943ab694401ffc1f9e46555a7
mutec <florian@mysterycode.de>
2016-01-14 15:13:24 +0100

    clear content->contentData->compiled on update

--- files/acp/update202.php
+++ files/acp/update202.php
@@ -1,5 +1,6 @@
 <?php
 use cms\data\content\ContentAction;
+use cms\data\content\ContentEditor;
 use cms\data\content\ContentList;
 use wcf\data\object\type\ObjectTypeCache; 
 
@@ -21,3 +22,23 @@ $affectedObjects = $list->getObjects();
 //delete all headlines
 $action = new ContentAction($affectedObjects, 'delete', array());
 $action->executeAction();
+
+$id = ObjectTypeCache::getInstance()->getObjectTypeIDByName('de.codequake.cms.content.type', 'de.codequake.cms.content.type.template');
+$list = new ContentList();
+$list->getConditionBuilder()->add('content.contentTypeID', array($id));
+$list->readObjects();
+$affectedObjects = $list->getObjects();
+
+// clear compiled
+foreach ($affectedObjects as $content) {
+	if (!empty($content->compiled)) {
+		$contentData = $content->contentData;
+		$tmpArray = unserialze($contentData);
+		unset($tmpArray['compiled']);
+		$contentData = serialze($tmpArray);
+		
+		$contentEditor = new ContentEditor($content);
+		$contentEditor->update(array('contentData' => $contentData));
+	}
+}
+
