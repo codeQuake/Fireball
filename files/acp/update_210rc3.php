7cb090cb33cf2e290c32df7f131c7e1dc6868cf1
mutec <webmaster@mysterycode.de>
2016-05-21 20:36:53 +0200

    format code

--- files/acp/update_210rc3.php
+++ files/acp/update_210rc3.php
@@ -8,45 +8,50 @@ use wcf\data\object\type\ObjectTypeCache;
  *
  * @author Jens Krumsieck
  * @copyright 2013 - 2015 codeQuake
- * @license GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
+ * @license GNU Lesser General Public License
+ *          <http://www.gnu.org/licenses/lgpl-3.0.txt>
  * @package de.codequake.cms
  */
 
-$id = ObjectTypeCache::getInstance ()->getObjectTypeIDByName ( 'de.codequake.cms.content.type', 'de.codequake.cms.content.type.headline' );
-$list = new ContentList ();
-$list->getConditionBuilder ()->add ( 'content.position', array (
-		'sidebar' 
-) );
-$list->getConditionBuilder ()->add ( 'content.contentTypeID', array (
-		$id 
-) );
-$list->readObjects ();
+$id = ObjectTypeCache::getInstance()->getObjectTypeIDByName(
+        'de.codequake.cms.content.type', 
+        'de.codequake.cms.content.type.headline');
+$list = new ContentList();
+$list->getConditionBuilder()->add('content.position', array(
+        'sidebar'
+));
+$list->getConditionBuilder()->add('content.contentTypeID', array(
+        $id
+));
+$list->readObjects();
 
-$affectedObjects = $list->getObjects ();
+$affectedObjects = $list->getObjects();
 
 // delete all headlines
-$action = new ContentAction ( $affectedObjects, 'delete', array () );
-$action->executeAction ();
+$action = new ContentAction($affectedObjects, 'delete', array());
+$action->executeAction();
 
-$id = ObjectTypeCache::getInstance ()->getObjectTypeIDByName ( 'de.codequake.cms.content.type', 'de.codequake.cms.content.type.template' );
-$list = new ContentList ();
-$list->getConditionBuilder ()->add ( 'content.contentTypeID', array (
-		$id 
-) );
-$list->readObjects ();
-$affectedObjects = $list->getObjects ();
+$id = ObjectTypeCache::getInstance()->getObjectTypeIDByName(
+        'de.codequake.cms.content.type', 
+        'de.codequake.cms.content.type.template');
+$list = new ContentList();
+$list->getConditionBuilder()->add('content.contentTypeID', array(
+        $id
+));
+$list->readObjects();
+$affectedObjects = $list->getObjects();
 
 // clear compiled
-foreach ( $affectedObjects as $content ) {
-	if (! empty ( $content->compiled )) {
-		$contentData = $content->contentData;
-		$tmpArray = unserialze ( $contentData );
-		unset ( $tmpArray ['compiled'] );
-		$contentData = serialze ( $tmpArray );
-		
-		$contentEditor = new ContentEditor ( $content );
-		$contentEditor->update ( array (
-				'contentData' => $contentData 
-		) );
-	}
+foreach ($affectedObjects as $content) {
+    if (! empty($content->compiled)) {
+        $contentData = $content->contentData;
+        $tmpArray = unserialze($contentData);
+        unset($tmpArray['compiled']);
+        $contentData = serialze($tmpArray);
+        
+        $contentEditor = new ContentEditor($content);
+        $contentEditor->update(array(
+                'contentData' => $contentData
+        ));
+    }
 }
