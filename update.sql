716cd8b9913c3e42c4a54c1cff83bb15faab6874
mutec <webmaster@mysterycode.de>
2016-05-22 04:33:43 +0200

    change column filesize
    resolves #155

--- update.sql
+++ update.sql
@@ -1,9 +1,1 @@
-ALTER TABLE cms1_page CHANGE isHome isHome TINYINT(1) NOT NULL DEFAULT 0;
-ALTER TABLE cms1_page CHANGE clicks clicks INT (10) NOT NULL DEFAULT 0;
-ALTER TABLE cms1_page CHANGE showOrder showOrder INT(10) NOT NULL DEFAULT 0;
-ALTER TABLE cms1_page CHANGE invisible invisible TINYINT(1) NOT NULL DEFAULT 0;
-ALTER TABLE cms1_page CHANGE availableDuringOfflineMode availableDuringOfflineMode TINYINT(1) NOT NULL DEFAULT 0;
-
-ALTER TABLE cms1_counter CHANGE visits visits INT(10) NOT NULL DEFAULT 0;
-ALTER TABLE cms1_counter CHANGE users users INT(10) NOT NULL DEFAULT 0;
-ALTER TABLE cms1_counter CHANGE spiders spiders INT(10) NOT NULL DEFAULT 0;
+ALTER TABLE cms1_file CHANGE fileSize filesize INT(10) NOT NULL DEFAULT 0;
