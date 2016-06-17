3e3808bf785cba950b04c5f845463d9fb3e74323
mutec <webmaster@mysterycode.de>
2016-06-17 15:05:11 +0200

    basic page importer

---
+++ files/lib/system/importer/PageImporter.class.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace cms\system\importer;
+use wcf\system\importer\AbstractImporter;
+use wcf\system\importer\ImportHandler;
+use cms\data\page\Page;
+use cms\data\page\PageAction;
+
+/**
+ * Provides an importer for pages
+ * 
+ * @author	Florian Gail
+ * @copyright	2013 - 2016 codeQuake
+ * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
+ * @package	de.codequake.cms
+ */
+class PageImporter extends AbstractImporter {
+	/**
+	 * @see	\wcf\system\importer\AbstractImporter::$className
+	 */
+	protected $className = 'cms\data\page\Page';
+	
+	/**
+	 * @see	\wcf\system\importer\IImporter::import()
+	 */
+	public function import($oldID, array $data, array $additionalData = array()) {
+		$data['userID'] = ImportHandler::getInstance()->getNewID('com.woltlab.wcf.user', $data['userID']);
+		
+		if (is_numeric($oldID)) {
+			$page = new Page($oldID);
+			if (!$page->pageID)
+				$data['pageID'] = $oldID;
+		}
+		
+		$action = new PageAction(array(), 'create', array(
+			'data' => $data
+		));
+		$returnValues = $action->executeAction();
+		$newID = $returnValues['returnValues']->pageID;
+		$page = new Page($newID);
+		
+		ImportHandler::getInstance()->saveNewID('de.codequake.cms.page', $oldID, $page->pageID);
+		
+		return $page->pageID;
+	}
+}
