0b87a6f3472e1e94690bdfb2bbe01a676bfca3e1
mutec <webmaster@mysterycode.de>
2016-06-28 17:52:28 +0200

    auto guess the home page
    i thought i already commited sth like that

--- files/lib/system/request/PageRoute.class.php
+++ files/lib/system/request/PageRoute.class.php
@@ -139,6 +139,15 @@ class PageRoute implements IRoute {
 	 */
 	public function matches($requestURL) {
 		$controller = $this->getControllerName();
+		
+		$this->landingPage = PageMenu::getInstance()->getLandingPage();
+		$processor = $this->landingPage->getProcessor();
+		if ($processor instanceof CMSPageMenuItemProvider) {
+			$page = $processor->getPage();
+			$alias = $page->getAlias();
+			$this->routeData['alias'] = $alias;
+			return true;
+		}
 
 		if (!URL_LEGACY_MODE) {
 			// request URL must be prefixed with `page/`
