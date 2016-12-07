781b58c1494e41e89ab8670c32a7eafb8e9c4398
mutec <webmaster@mysterycode.de>
2016-12-07 18:47:01 +0100

    initiating fireball namespace (JS)
    i forget this part every time...

--- files/js/Fireball.js
+++ files/js/Fireball.js
@@ -6,7 +6,7 @@
  * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
  * @package	de.codequake.cms
  */
-if (!CMS) var CMS = {};
+if (!Fireball) var Fireball = {};
 
 $.widget('ui.fireSlide', {
 	/**
