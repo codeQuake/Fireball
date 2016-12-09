989dcd1cb795fd9739e5ec6a9f51e03222709a10
mutec <webmaster@mysterycode.de>
2016-12-10 00:26:16 +0100

    catch parse errors on php contents

--- files/lib/system/content/type/PHPContentType.class.php
+++ files/lib/system/content/type/PHPContentType.class.php
@@ -2,6 +2,8 @@
 namespace cms\system\content\type;
 
 use cms\data\content\Content;
+use wcf\system\request\LinkHandler;
+use wcf\system\WCF;
 
 /**
  * @author	Jens Krumsieck
@@ -14,7 +16,7 @@ class PHPContentType extends AbstractContentType {
 	 * @see	\cms\system\content\type\AbstractContentType::$icon
 	 */
 	protected $icon = 'fa-code';
-	
+
 	/**
 	 * @see	\cms\system\content\type\AbstractContentType::$previewFields
 	 */
@@ -29,6 +31,22 @@ class PHPContentType extends AbstractContentType {
 	 * @see	\cms\system\content\type\IContentType::getOutput()
 	 */
 	public function getOutput(Content $content) {
-		return eval($content->text);
+		try {
+			$output = eval($content->text);
+		}
+		catch (\ParseError $e) {
+			if (WCF::getSession()->getPermission('admin.fireball.content.canAddContent')) {
+				$url = LinkHandler::getInstance()->getLink('ContentEdit', array('application' => 'cms', 'object' => $content, 'isACP' => true));
+				$output = '<div class="error">';
+				$output .= 'Please check <a href="' . $url . '">content #' . $content->contentID . '</a>. The following error occurred parsing this content at line ' . $e->getLine() . ':<br><br>';
+				$output .= $e->getMessage();
+				$output .= '</div>';
+			}
+			else {
+				$output = '';
+			}
+		}
+
+		return $output;
 	}
 }
