195643a18d6a76d717d8a63ceb8c748af0c5fefc
mutec <webmaster@mysterycode.de>
2016-12-11 02:58:20 +0100

    fix template name
    (cherry picked from commit 62a9cee)

--- files/lib/data/page/PageAction.class.php
+++ files/lib/data/page/PageAction.class.php
@@ -40,14 +40,19 @@ class PageAction extends AbstractDatabaseObjectAction implements IClipboardActio
 	protected $className = 'cms\data\page\PageEditor';
 
 	/**
+	 * @see	\wcf\data\AbstractDatabaseObjectAction::$permissionsCreate
+	 */
+	protected $permissionsCreate = array('admin.fireball.page.canAddPage');
+	
+	/**
 	 * @see	\wcf\data\AbstractDatabaseObjectAction::$permissionsDelete
 	 */
-	protected $permissionsDelete = array('admin.cms.page.canAddPage');
+	protected $permissionsDelete = array('admin.fireball.page.canAddPage');
 
 	/**
 	 * @see	\wcf\data\AbstractDatabaseObjectAction::$permissionsUpdate
 	 */
-	protected $permissionsUpdate = array('admin.cms.page.canAddPage');
+	protected $permissionsUpdate = array('admin.fireball.page.canAddPage');
 
 	/**
 	 * @see	\wcf\data\AbstractDatabaseObjectAction::$requireACP
@@ -338,7 +343,261 @@ class PageAction extends AbstractDatabaseObjectAction implements IClipboardActio
 
 		foreach ($this->objects as $pageEditor) {
 			$pageEditor->update(array('isDisabled' => 0));
+
+			$menuItem = $pageEditor->getMenuItem();
+			if ($menuItem !== null) {
+				$menuItemEditor = new MenuItemEditor($pageEditor->getMenuItem());
+				$menuItemEditor->update(array('isDisabled' => 0));
+			}
 		}
+	}
+	
+	/**
+	 * Validates parameters to create a page in frontend
+	 */
+	 public function validateFrontendCreate() {
+		//check permission
+		if (!WCF::getSession()->getPermission('admin.fireball.page.canAddPage')) throw new AJAXException();
+		//TODO I18n & other error handling
+		if (!isset($this->parameters['data']['parentID'])) $this->parameters['data']['parentID'] = null;
+
+		//validate alias
+		if ($this->parameters['data']['alias'] == '') {
+			$this->parameters['data']['alias'] = PageUtil::buildAlias($this->parameters['data']['title']);
+		}
+		if (!PageUtil::isValidAlias($this->parameters['data']['alias'])) {
+			throw new UserInputException('alias', 'notValid');
+		}
+		if (!PageUtil::isAvailableAlias($this->parameters['data']['alias'], isset($this->parameters['data']['parentID']) ? $this->parameters['data']['parentID'] : null)) {
+			throw new UserInputException('alias', 'notUnique');
+		}
+
+		//validate sidebarOrientation
+		if (!in_array($this->parameters['data']['sidebarOrientation'], array('left', 'right'))) {
+			// force default value if invalid sidebar orientation
+			// specified
+			$this->parameters['data']['sidebarOrientation'] = 'right';
+		}
+
+		// validate parent page
+		if ($this->parameters['data']['parentID']) {
+			$parentPage = PageCache::getInstance()->getPage($this->parameters['data']['parentID']);
+			if ($parentPage === null) {
+				$this->parameters['data']['parentID'] = null;
+			}
+		}
+
+		// validate publication date
+		if ($this->parameters['data']['enableDelayedPublication']) {
+			$publicationDateTimestamp = @strtotime($this->parameters['data']['publicationDate']);
+			if ($publicationDateTimestamp === false || $publicationDateTimestamp <= TIME_NOW) {
+				throw new UserInputException('publicationDate', 'notValid');
+			}
+			// integer overflow
+			if ($publicationDateTimestamp > 2147483647) {
+				throw new UserInputException('publicationDate', 'notValid');
+			}
+		}
+
+		// validate deactivation date
+		if ($this->parameters['data']['enableDelayedDeactivation']) {
+			$deactivationDateTimestamp = @strtotime($this->parameters['data']['deactivationDate']);
+			if ($deactivationDateTimestamp === false || $deactivationDateTimestamp <= TIME_NOW) {
+				throw new UserInputException('deactivationDate', 'notValid');
+			}
+			// integer overflow
+			if ($deactivationDateTimestamp > 2147483647) {
+				throw new UserInputException('deactivationDate', 'notValid');
+			}
+			// deactivation date needs to be after publication date
+			if ($this->enableDelayedPublication && $deactivationDateTimestamp < $publicationDateTimestamp) {
+				throw new UserInputException('deactivationDate', 'beforePublication');
+			}
+		}
+
+		// validate menu item
+		if ($this->parameters['data']['createMenuItem']) {
+			$this->parameters['data']['menuItemID'] = 0;
+		}
+		if ($this->parameters['data']['menuItemID']) {
+			$menuItem = new PageMenuItem($this->parameters['data']['menuItemID']);
+			if (!$menuItem->menuItemID) {
+				// silently ignore menu item, user shouldn't be
+				// able to select this menu item in first place
+				$this->parameters['data']['menuItemID'] = 0;
+			}
+		}
+	 }
+
+	/**
+	 * performs frontend create
+	 */
+	public function frontendCreate() {
+		$data = $this->parameters['data'];
+		// publication
+		if ($data['enableDelayedPublication']) {
+			$data['isPublished'] = 0;
+			$dateTime = \DateTime::createFromFormat('Y-m-d H:i', $data['publicationDate'], WCF::getUser()->getTimeZone());
+			$data['publicationDate'] = $dateTime->getTimestamp();
+		} else $data['publicationDate'] = 0;
+		if ($data['enableDelayedDeactivation']) {
+			$dateTime = \DateTime::createFromFormat('Y-m-d H:i', $data['deactivationDate'], WCF::getUser()->getTimeZone());
+			$data['deactivationDate'] = $dateTime->getTimestamp();
+		}
+		else $data['deactivationDate'] = 0;
+
+		//get stylesheetIDs
+		$stylesheetIDs = array();
+		if (isset($data['stylesheetIDs'])) $stylesheetIDs = $data['stylesheetIDs'];
+
+		//check foreigns
+		if ($data['menuItemID'] == 0) $data['menuItemID'] = null;
+		if ($data['parentID'] == 0) $data['parentID'] = null;
+		if ($data['styleID'] == 0) $data['styleID'] = null;
+
+		//unset useless parameters
+		unset($data['enableDelayedPublication']);
+		unset($data['enableDelayedDeactivation']);
+		unset($data['createMenuItem']);
+		unset($data['t']);
+		unset($data['stylesheetIDs']);
+
+		//unset bullshit
+		unset($data['undefined']);
+
+		//set params
+		$this->parameters['stylesheetIDs'] = $stylesheetIDs;
+		$this->parameters['data'] = $data;
+
+		//finally create new page
+		$page = $this->create();
+		return array(
+			'page' => $page,
+			'link' => $page->getLink()
+			);
+	}
+
+	/**
+	 * Validates parameters to get the page add dialog.
+	 */
+	public function validateGetAddDialog() {
+		//check permission
+		if (!WCF::getSession()->getPermission('admin.fireball.page.canAddPage')) throw new AJAXException();
+	}
+
+	/**
+	 * Returns the page add dialog.
+	 */
+	public function getAddDialog() {
+		if (!empty($this->objectIDs)) {
+			//pageID = 0 -> add
+			if (reset($this->objectIDs) == 0) {
+				$action = 'add';
+				$page = null;
+			}
+			
+			//valid object -> edit
+			else if ($this->getSingleObject()) {
+				$action = 'edit';
+				$page = $this->getSingleObject();
+			}
+			
+			//bullshit given
+			else throw new AJAXException();
+		}
+		else {
+			$action = 'add';
+			$page = null;
+		}
+		
+		//default date
+		$dateTime = DateUtil::getDateTimeByTimestamp(TIME_NOW);
+		$dateTime->setTimezone(WCF::getUser()->getTimeZone());
+		$defaultDate = $dateTime->format('c');
+		
+		//get initial data
+		$alias = $page ? $page->alias : '';
+		$allowIndexing = $page ? $page->allowIndexing : FIREBALL_PAGES_DEFAULT_ALLOW_INDEXING;
+		$allowSubscribing = $page ? $page->allowSubscribing : FIREBALL_PAGES_DEFAULT_ALLOW_SUBSCRIBING;
+		$availableDuringOfflineMode = $page ? $page->availableDuringOfflineMode : FIREBALL_PAGES_DEFAULT_OFFLINE;
+		$createMenuItem = FIREBALL_PAGES_DEFAULT_MENU_ITEM;
+		$deactivationDate = $page ? $page->deactivationDate : $defaultDate;
+		$description = $page ? $page->description : '';
+		$enableDelayedDeactivation = $page ? $page->enableDelayedDeactivation : 0;
+		$enableDelayedPublication = $page ? $page->enableDelayedPublication : 0;
+		$invisible = $page ? $page->invisible : 0;
+		$isCommentable = $page ? $page->isCommentable : FIREBALL_PAGES_DEFAULT_COMMENTS;
+		$menuItemID = $page ? $page->menuItemID : 0;
+		$metaDescription = $page ? $page->metaDescription : '';
+		$metaKeywords = $page ? $page->metaKeywords : '';
+		$pageID = $page ? $page->pageID : 0;
+		$publicationDate = $page ? $page->publicationDate : $defaultDate;
+		$parentID = $page ? $page->parentID : 0;
+		$showOrder = $page ? $page->showOrder : 0;
+		$sidebarOrientation = $page ? $page->sidebarOrientation : FIREBALL_PAGES_DEFAULT_SIDEBAR;
+		$styleID = $page ? $page->styleID : 0;
+		$stylesheetIDs = $page ? $page->getStylesheetIDs() : array();
+		$title = $page ? $page->title : '';
+		
+		//read data
+		$stylesheetList = new StylesheetList();
+		$stylesheetList->readObjects();
+		$stylesheetList = $stylesheetList->getObjects();
+		$availableStyles = StyleHandler::getInstance()->getStyles();
+		$pageNodeTree = new PageNodeTree();
+		$pageList = $pageNodeTree->getIterator();
+		
+		// load menu items
+		$menuItemList = new PageMenuItemList();
+		$menuItemList->getConditionBuilder()->add('page_menu_item.menuPosition = ?', array('header'));
+		$menuItemList->sqlOrderBy = 'page_menu_item.parentMenuItem ASC, page_menu_item.showOrder ASC';
+		$menuItemList->readObjects();
+		foreach ($menuItemList as $menuItem) {
+			if ($menuItem->parentMenuItem) {
+				if (isset($menuItems[$menuItem->parentMenuItem])) {
+					$menuItems[$menuItem->parentMenuItem]->addChild($menuItem);
+				}
+			} else {
+				$menuItems[$menuItem->menuItem] = new ViewablePageMenuItem($menuItem);
+			}
+		}
+		
+		WCF::getTPL()->assign(array(
+			'action' => $action,
+			'alias' => $alias,
+			'allowIndexing' => $allowIndexing,
+			'allowSubscribing' => $allowSubscribing,
+			'availableDuringOfflineMode' => $availableDuringOfflineMode,
+			'createMenuItem' => $createMenuItem,
+			'deactivationDate' => $deactivationDate,
+			'description' => $description,
+			'enableDelayedDeactivation' => $enableDelayedDeactivation,
+			'enableDelayedPublication' => $enableDelayedPublication,
+			'invisible' => $invisible,
+			'isCommentable' => $isCommentable,
+			'menuItemID' => $menuItemID,
+			'metaDescription' => $metaDescription,
+			'metaKeywords' => $metaKeywords,
+			'publicationDate' => $publicationDate,
+			'parentID' => $parentID,
+			'showOrder' => $showOrder,
+			'sidebarOrientation' => $sidebarOrientation,
+			'styleID' => $styleID,
+			'stylesheetIDs' => $stylesheetIDs,
+			'title' => $title,
+			
+			'stylesheetList' => $stylesheetList,
+			'availableStyles' => $availableStyles,
+			'pageList' => $pageList,
+			'action' => $action,
+			'pageID' => $pageID,
+			'menuItems' => $menuItems
+		));
+
+		return array(
+			'pageID' => $pageID,
+			'template' => WCF::getTPL()->fetch('pageAddDialog', 'cms')
+		);
 	}
 
 	/**
@@ -389,6 +648,35 @@ class PageAction extends AbstractDatabaseObjectAction implements IClipboardActio
 			'parentID' => isset($this->parameters['parentID']) ? intval($this->parameters['parentID']) : null,
 			'position' => $this->parameters['position'],
 			'template' => WCF::getTPL()->fetch('contentTypeList', 'cms')
+		);
+	}
+
+
+
+	/**
+	 * Validates parameters to get a rendered list of contents.
+	 */
+	public function validateGetSortableContentList() {
+		$this->readString('position');
+		if (!in_array($this->parameters['position'], array('body', 'sidebar', 'sidebarLeft', 'sidebarRight'))) {
+			throw new UserInputException('position');
+		}
+
+		// validate 'objectIDs' parameter
+		$this->getSingleObject();
+	}
+
+	public function getSortableContentList() {
+		/** @var $page Page */
+		$page = $this->getSingleObject();
+		$position = $this->parameters['position'];
+
+		return array(
+			'position' => $position,
+			'template' => WCF::getTPL()->fetch('sortableContentList', 'cms', array(
+				'contentNodeTree' => $page->getContents()[$position],
+				'position' => $position
+			))
 		);
 	}
 
@@ -507,7 +795,7 @@ class PageAction extends AbstractDatabaseObjectAction implements IClipboardActio
 	 */
 	public function validateSetAsHome() {
 		WCF::getSession()->checkPermissions(array(
-			'admin.cms.page.canAddPage'
+			'admin.fireball.page.canAddPage'
 		));
 
 		// validate 'objectIDs' parameter
