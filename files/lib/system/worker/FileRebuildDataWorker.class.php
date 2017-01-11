2940e1d744c02226952c18f8dea97d0e15528323
mutec <webmaster@mysterycode.de>
2017-01-11 04:51:50 +0100

    rebuild worker for files
    worker generates thumbnails

---
+++ files/lib/system/worker/FileRebuildDataWorker.class.php
@@ -0,0 +1,40 @@
+<?php
+namespace cms\system\worker;
+
+use cms\data\file\FileAction;
+use wcf\system\worker\AbstractRebuildDataWorker;
+
+/**
+ * Worker implementation for files.
+ * 
+ * @author	Jens Krumsieck
+ * @copyright	2013 - 2015 codeQuake
+ * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
+ * @package	de.codequake.cms
+ */
+class FileRebuildDataWorker extends AbstractRebuildDataWorker {
+	/**
+	 * @see	\wcf\system\worker\AbstractWorker::$limit
+	 */
+	protected $limit = 100;
+
+	/**
+	 * @see	\wcf\system\worker\AbstractRebuildDataWorker::$objectListClassName
+	 */
+	protected $objectListClassName = 'cms\data\file\FileList';
+
+	/**
+	 * @see	\wcf\system\worker\IWorker::execute()
+	 */
+	public function execute() {
+		parent::execute();
+
+		/** @var \cms\data\file\File $file */
+		foreach ($this->objectList->getObjects() as $file) {
+			if ($file->isImage() && !$file->hasThumbnail()) {
+				$fileAction = new FileAction(array($file), 'generateThumbnail');
+				$fileAction->executeAction();
+			}
+		}
+	}
+}
