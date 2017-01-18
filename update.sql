f593843861166b5a24ced618e5e9697661ab0164
mutec <webmaster@mysterycode.de>
2017-01-18 04:23:11 +0100

    fixed scss column type

--- update.sql
+++ update.sql
@@ -1,4 +1,4 @@
-ALTER TABLE cms1_stylesheet ADD scss INT(10) NOT NULL DEFAULT 0;
+ALTER TABLE cms1_stylesheet ADD scss MEDIUMTEXT;
 
 ALTER TABLE cms1_content_to_dashboardbox DROP FOREIGN KEY contentID;
 DROP TABLE cms1_content_to_dashboardbox;
