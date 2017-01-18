de017b7eee70cbdd5be098ee790cc7fd896bff81
mutec <webmaster@mysterycode.de>
2017-01-18 03:21:55 +0100

    remove foreign key

--- update.sql
+++ update.sql
@@ -1,2 +1,4 @@
 ALTER TABLE cms1_stylesheet ADD scss INT(10) NOT NULL DEFAULT 0;
-DROP TABLE cms1_content_to_box;
+
+ALTER TABLE cms1_content_to_dashboardbox DROP FOREIGN KEY contentID;
+DROP TABLE cms1_content_to_dashboardbox;
