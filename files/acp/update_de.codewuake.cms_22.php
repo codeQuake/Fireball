70c8fe1d34a76557382922bebe693bd1373c25dd
mutec <webmaster@mysterycode.de>
2017-01-18 01:59:35 +0100

    3.0.0 Beta 1

--- files/acp/update_de.codewuake.cms_22.php
+++ files/acp/update_de.codewuake.cms_22.php
@@ -1,5 +1,7 @@
 <?php
 
+use cms\data\content\ContentEditor;
+use cms\data\content\ContentList;
 use cms\data\stylesheet\StylesheetEditor;
 use cms\data\stylesheet\StylesheetList;
 
@@ -7,7 +9,25 @@ $stylesheetList = new StylesheetList();
 $stylesheetList->readObjects();
 $stylesheets = $stylesheetList->getObjects();
 
+/** @var \cms\data\stylesheet\Stylesheet $stylesheet */
 foreach ($stylesheets as $stylesheet) {
 	$stylesheetEditor = new StylesheetEditor($stylesheet);
 	$stylesheetEditor->update(['scss' => $stylesheet->less]);
+}
+
+$contentList = new ContentList();
+$contentList->readObjects();
+$contents = $contentList->getObjects();
+/** @var \cms\data\content\Content $content */
+foreach ($contents as $content) {
+	$typeName = $content->getTypeName();
+	if ($typeName != 'de.codequake.cms.content.type.headline' || $typeName != 'de.codequake.cms.content.type.dashboard'
+		|| $typeName != 'de.codequake.cms.content.type.user' || $typeName != 'de.codequake.cms.content.type.group'
+		|| $typeName != 'de.codequake.cms.content.type.tabmenu' || $typeName != 'de.codequake.cms.content.type.columns'
+		|| $typeName != 'de.codequake.cms.content.type.wsipimport') {
+		continue;
+	}
+
+	$contentEditor = new ContentEditor($content);
+	$contentEditor->update(['showHeadline' => 1]);
 }
